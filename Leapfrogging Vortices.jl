6f00fd8bb9ec730d99c28fb6f01122003d319249
Steven McLain <stevenmclain@Stevens-MBP-5.sumo.io>
2019-06-02 16:02:25 -0600

    Ready to begin

--- Leapfrogging Vortices.jl
+++ Leapfrogging Vortices.jl
@@ -2,7 +2,5 @@
 # Author : Steven McLain
 # Date : May 27, 2019
 # Git Version History Link: https://github.com/stevenm89/Leapfrogging-Vortices.git
-
-x = 1;
-# Add Comment testing git 
+# User: stevenm89
 
