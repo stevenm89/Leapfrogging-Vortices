3cbadc0c0cdc390d76559333c2d67233b40a5798
Steven McLain <stevenmclain@Stevens-MBP-5.sumo.io>
2019-06-23 08:14:57 -0600

    Update 2.0

--- Leapfrogging Vortices.jl
+++ Leapfrogging Vortices.jl
@@ -4,3 +4,15 @@
 # Git Version History Link: https://github.com/stevenm89/Leapfrogging-Vortices.git
 # User: stevenm89
 
+#Input
+r = 1; #Radius of vortex
+circ = 4; #Cirulation or strength of vortex
+
+#Output
+tanvel = circ/(2*pi*r) #Tangential velocity of vortex
+println("Radius of vortex:")
+println(r)
+println("Cirulation or strength of vortex:")
+println(circ)
+println("Tangential velocity of vortex:")
+println(tanvel)
--- README.md
+++
@@ -1,1 +0,0 @@
-# Leapfrogging-Vortices
