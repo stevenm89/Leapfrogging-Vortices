437c188e643b1859be5a61f4e93163047ae493c4
Steven McLain <stevenmclain@Stevens-MBP-5.sumo.io>
2019-06-27 21:43:38 -0600

    Getting there

--- Leapfrogging Vortices.jl
+++ Leapfrogging Vortices.jl
@@ -3,14 +3,16 @@
 # Date : May 27, 2019
 # Git Version History Link: https://github.com/stevenm89/Leapfrogging-Vortices.git
 # User: stevenm89
+using LinearAlgebra
 
 #Input
-r = 1; #Radius of vortex
-circ = 4; #Cirulation or strength of vortex
+r = [0;1;0]; #Distance between Vortices
+rnorm = norm(r)
+circ = [0;0;1]; #Cirulation or strength of vortex
 
 #Output
-tanvel = circ/(2*pi*r) #Tangential velocity of vortex
-println("Radius of vortex:")
+tanvel = cross(circ, r)/(2*pi*rnorm^2) #Tangential velocity of vortex
+println("Distance between Vortices:")
 println(r)
 println("Cirulation or strength of vortex:")
 println(circ)
