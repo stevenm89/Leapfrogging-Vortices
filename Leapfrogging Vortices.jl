26c9974f79ff9046d027680d4bd7517e7ccf0230
Steven McLain <stevenmclain@Stevens-MBP-5.sumo.io>
2019-07-04 10:46:28 -0600

    Getting the right numbers, but adding them wrong.

--- Leapfrogging Vortices.jl
+++ Leapfrogging Vortices.jl
@@ -1,4 +1,4 @@
-# FLOW Lab LTRAD Program
+# FLOW Lab - Leapfrogging Vortices
 # Author : Steven McLain
 # Date : May 27, 2019
 # Git Version History Link: https://github.com/stevenm89/Leapfrogging-Vortices.git
@@ -6,13 +6,13 @@
 using LinearAlgebra
 
 #Input
-r = [0;1;0]; #Distance between Vortices
+r = [1; -1; 0]; #Radius from effected vortex under examination to other vortex
 rnorm = norm(r)
-circ = [0;0;1]; #Cirulation or strength of vortex
+circ = [0; 0; 1]; #Cirulation or strength of vortex
 
 #Output
 tanvel = cross(circ, r)/(2*pi*rnorm^2) #Tangential velocity of vortex
-println("Distance between Vortices:")
+println("Radii between Vortices:")
 println(r)
 println("Cirulation or strength of vortex:")
 println(circ)
