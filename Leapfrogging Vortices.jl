0cb005426e4e0682bb79ff706999d12f0acf2999
Steven McLain <stevenmclain@Stevens-MBP-5.sumo.io>
2019-07-11 22:47:53 -0600

    Does 1 push back? Doesn't make sense because that's 0.

--- Leapfrogging Vortices.jl
+++ Leapfrogging Vortices.jl
@@ -5,16 +5,34 @@
 # User: stevenm89
 using LinearAlgebra
 
-#Input
-r = [1; -1; 0]; #Radius from effected vortex under examination to other vortex
-rnorm = norm(r)
-circ = [0; 0; 1]; #Cirulation or strength of vortex
+#Input ------------------------------------------------------------
+r2to1 = [0,-1,0]; #Enter initial vector distance of particle 2 to 1 (same vortex ring)
+r3to1 = [1,-1,0]; #Enter initial vector distance of particle 3 to 1 (other vortex ring)
+r4to1 = [1,0,0]; #Enter initial vector distance of particle 4 to 1 (other vortex ring)
 
-#Output
-tanvel = cross(circ, r)/(2*pi*rnorm^2) #Tangential velocity of vortex
-println("Radii between Vortices:")
-println(r)
-println("Cirulation or strength of vortex:")
-println(circ)
-println("Tangential velocity of vortex:")
-println(tanvel)
+circ = [0; 0; 1]; #Cirulation or strength of vortex (this means x is pos to left, y up, z into page)
+x = 1; #Number of Time Steps Desired
+
+#Output -----------------------------------------------------------
+rnorm2to1 = norm(r2to1);
+rnorm3to1 = norm(r3to1);
+rnorm4to1 = norm(r4to1);
+
+#for n2 = x
+    tanvel2on1 = cross(circ, r2to1)/(2*pi*rnorm2to1^2); #Tangential velocity of vortex 2 acting on 1
+    tanvel3on1 = cross(circ, r3to1)/(2*pi*rnorm3to1^2); #Tangential velocity of vortex 3 acting on 1
+    tanvel4on1 = cross(circ, r4to1)/(2*pi*rnorm4to1^2); #Tangential velocity of vortex 4 acting on 1
+    tanvelon1 = tanvel2on1 + tanvel3on1 + tanvel4on1;
+#end
+# #---------In process of edit
+# r1to2 = [0,1,0];
+# r1to3 = [-1,1,0];
+# r1to4 = [-1,0,0]; 
+# tanvel1on2 = cross(circ, r1to2)/(2*pi*rnorm2to1^2);
+# tanvel1on3 = cross(circ, r1to3)/(2*pi*rnorm3to1^2);
+# tanvel1on4 = cross(circ, r1to4)/(2*pi*rnorm4to1^2); 
+# tanvelfrom1 = tanvel1on2 + tanvel1on3 + tanvel1on4;
+# difference = tanvelon1 + tanvelfrom1;
+# #---------
+println("Tangential velocity of vortex 1:")
+#println(difference)
